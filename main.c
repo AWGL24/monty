95b13807a219e7ea1026701e9d06173b00b1849a
Drew-24 <andrewwil1@hotmail.com>
2022-01-12 15:16:05 +0000

    Added func handler function

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int main(int ac, char **av)
 {
     if (ac == 2)
-        open_and_handle_function(av[1]);
+        func_handle(av[1]);
     else
     {
         dprintf("Error: Can't open file %s\n", av[0]);
