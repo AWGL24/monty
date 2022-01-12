053c75cfeb6a68bc077fbdddd10cd51999861fa1
Drew-24 <andrewwil1@hotmail.com>
2022-01-12 13:50:52 +0000

    added new pint function, but it needs more refinement and code

---
+++ pint.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ *
+ *
+ */
+void _pint(stack_t )
+{
+    if (stack == NULL || *stack == NULL)
+    {
+        print ("error")
+            exit (FAILURE)
+
+    }
+    print(""%i" (*stack)->n)
+
+}
