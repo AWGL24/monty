139a9e5778598177ab42769959b50dee0a66a600
Soly-M-27 <solymar227@icloud.com>
2022-01-14 02:41:32 +0000

    Adding push.c function

--- push.c
+++ push.c
@@ -1,19 +1,26 @@
 #include "monty.h"
 int value;
+
 /**
  * _push - function that pushes an element to the stack
  * @stack: 
  *
  */
-void _push(stack_t **stack, unsigned int line_num)
+
+void _push(stack_t **stack, unsigned int line)
 {
-    stack_t *new_node = NULL;
-    (void)line_num;
+	stack_t *new_node = NULL;
+	(void)line;
 
-    new_node = node_creator(value);
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+		exit(EXIT_FAILURE);
 
-    new_node->next = *stack;
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	new_node->n = value;
+	new_node->next = *stack;
+	new_node->prev = NULL;
+
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
