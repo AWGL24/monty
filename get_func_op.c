1f7764ef3c5fae718dc9535366ccaa37512e2d41
Soly-M-27 <solymar227@icloud.com>
2022-01-17 21:30:53 +0000

    File that sends functions according to tokens in func_handler.c

--- get_func_op.c
+++ get_func_op.c
@@ -6,7 +6,6 @@ int get_func_op(stack_t **stack, char *func, unsigned int line)
 
 	/*printf("%s\n", func);*/
 	instruction_t funcs[] = {
-		{"push", _push},
 		{"pall", pall},
 		/*{"pint", pint},
 		{"pop", pop},
@@ -17,13 +16,12 @@ int get_func_op(stack_t **stack, char *func, unsigned int line)
 
 	while (funcs[x].opcode)
 	{
+		/*printf("From get_func_op function:\n");
 		printf("I am function %s\n", func);
-		printf("I am the selector %s\n", funcs[x].opcode);
+		printf("I am the selector %s\n", funcs[x].opcode);*/
 		if (strcmp(func, funcs[x].opcode) == 0)
 		{
-			printf("hello\n");
 			funcs[x].f(stack, line);
-			printf("Hi\n");
 			return (0);
 		}
 		x++;
