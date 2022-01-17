dd693c22fed4524b28f3c5b5419b236f21eaaa84
Soly-M-27 <solymar227@icloud.com>
2022-01-17 22:42:25 +0000

    Fixed betty

--- get_func_op.c
+++ get_func_op.c
@@ -1,24 +1,26 @@
 #include "monty.h"
 
+/**
+ * get_func_op - Function that retrieves function call and
+ * sends it to correct function
+ * @stack: stack containing the elements of doubly linked
+ * list
+ * @func: Name of function to be executed
+ * @line: Line number that you are currently on in the file
+ *
+ * Return: 0 if successful, -1 if failure occurs.
+ */
+
 int get_func_op(stack_t **stack, char *func, unsigned int line)
 {
 	int x = 0;
-
-	/*printf("%s\n", func);*/
 	instruction_t funcs[] = {
 		{"pall", pall},
-		/*{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"nop", nop},*/
 		{NULL, NULL}
 	};
 
 	while (funcs[x].opcode)
 	{
-		/*printf("From get_func_op function:\n");
-		printf("I am function %s\n", func);
-		printf("I am the selector %s\n", funcs[x].opcode);*/
 		if (strcmp(func, funcs[x].opcode) == 0)
 		{
 			funcs[x].f(stack, line);
@@ -27,8 +29,8 @@ int get_func_op(stack_t **stack, char *func, unsigned int line)
 		x++;
 	}
 
-	printf("Line %d: unknown instruction %s\n", line, func);
-	exit (EXIT_FAILURE);
+	printf("L%d: unknown instruction %s\n", line, func);
+	exit(EXIT_FAILURE);
 
 	return (-1);
 }
