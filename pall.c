809be0c849ec52379f1459c5d27bc25b27e559e8
Soly-M-27 <solymar227@icloud.com>
2022-01-17 21:29:27 +0000

    Function pall

--- pall.c
+++ pall.c
@@ -5,12 +5,14 @@
  */
 void pall(stack_t **stack, unsigned int count)
 {
-    stack_t *tmp = *stack;
-    (void)count;
+	stack_t *tmp = *stack;
+	(void)count;
 
-    while (tmp != NULL)
-    {
-        dprintf(STDOUT_FILENO, "%d\n", tmp->n);
-        tmp = tmp->next;
-    }
+	/*printf("I am in the pall function\n");*/
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+	/*printf("Pall finished\n");*/
 }
